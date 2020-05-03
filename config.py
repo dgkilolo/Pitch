7fe683f222e2f5f6e35d7952c6fd086c703af27f
David <dgkilolo23@gmail.com>
2020-05-03 21:23:30 +0300

    adds the general layout of the landing page.

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+
+class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dkg:boot@localhost/watchlist'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # SUBJECT_PREFIX = 'Watchlist'
+    # SENDER_EMAIL = 'james@moringaschool.com'
+
+    #simple mde configurations
+    # SIMPLEMDE_JS_IIFE = True
+    # SIMPLEMDE_USE_CDN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    
+    DEBUG = True
+
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dkg:boot@localhost/watchlist_test'
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+# 'test':TestConfig
+
+}
