0fc3b66b3abe9516f65e651899de921f29ae3d90
David <dgkilolo23@gmail.com>
2020-05-04 14:46:56 +0300

    send email to user once they sign in.

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from . import auth
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
-# from ..email import mail_message
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -29,7 +29,7 @@ def register():
     db.session.add(user)
     db.session.commit()
 
-    # mail_message("Welcome to Pitch","email/welcome_user",user.email,user=user)
+    mail_message("Welcome to Pitch","email/welcome_user",user.email,user=user)
 
     return redirect(url_for('auth.login'))
     title = "New Account"
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY='1234'
+export MAIL_USERNAME='dgkilolo23@gmail.com'
+export MAIL_PASSWORD='lamashit23'
 
 python3.6 manage.py server
